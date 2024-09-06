7925709c95b0b92ccb46763dcd18b7830b5d4ec5
ddastan <dm_temir@icloud.com>
2024-09-06 19:26:08 +0600

    hw1

---
+++ handlers/echo.py
@@ -0,0 +1,15 @@
+from config import Bot, dp
+from aiogram import types, Dispatcher
+
+
+
+async def echo_handler(message: types.Message):
+    if message.text.isdigit():
+        await message.answer(int(message.text)**2)
+    else:
+        await message.answer(message.text)
+
+
+
+def register_echo(dp: Dispatcher):
+    dp.register_message_handler(echo_handler )
